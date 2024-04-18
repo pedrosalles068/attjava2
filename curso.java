ae97dc05b57e0a691b87a4838c8ad589d06c7cd9
Deagleoh <vralnelas1@gmail.com>
2024-04-18 20:27:53 -0300

    penis

--- curso.java
+++ curso.java
@@ -5,4 +5,24 @@ public class curso {
     private String nome;
     private String descricao;
     private List<aluno> alunos = new ArrayList<>();
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getdescricao() {
+        return descricao;
+    }
+    public void listarAluno(aluno alunos) {
+        this.alunos.add(alunos);
+        aluno aluno1 = new aluno("213142");
+    }
+    public List<aluno> getaAlunos() {
+        return this.alunos;
+    }
+
 }
