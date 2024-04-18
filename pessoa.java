f978d2e2c89270225b25d13bc62b01e36d59f536
pedro <pedro.salles@alunos.unis.edu.br>
2024-04-18 19:18:58 -0300

    teste

--- pessoa.java
+++ pessoa.java
@@ -1,2 +1,31 @@
 public class pessoa {
+    private String nome;
+    private String dataDeNascimento;
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
+
+
