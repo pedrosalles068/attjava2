b1d4967dbb76797cfc6d3223d21c7c8ed8e51be9
Miguel Cunha <miguelcunha1205@gmail.com>
2024-04-18 20:19:18 -0300

    att

--- aluno.java
+++ aluno.java
@@ -1,2 +1,13 @@
 public class aluno {
+    private String matricula;
+
+    public aluno(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
